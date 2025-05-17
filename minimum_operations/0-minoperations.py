e92305e87abbdf29c70356a6d8770d72d2c9086c
Joellate <j.teta@alustudent.com>
2025-05-17 15:24:38 -0700

    task

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""
-Minimum Operations: Calculates the fewest number of operations needed
-to result in exactly n H characters using Copy All and Paste.
-"""
+"""minimum operations"""
+from math import sqrt
 
 
 def minOperations(n):
-    """Return minimum operations to reach n H's, else 0 if not possible."""
-    if n < 2:
+
+    if n <= 1:
         return 0
+    elif is_n_prime(n):
+        return n
+    else:
+        prime = small_prime(n)
+        if prime:
+            return prime + minOperations(int(n/prime))
 
-    operations = 0
-    factor = 2
 
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+def is_n_prime(n):
 
-    return operations
+    result = True
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            result = False
+    return result
 
+
+def small_prime(n):
+
+    result = None
+    for i in range(2, int(n/2 + 1)):
+        if n % i == 0:
+            result = i
+            break
+    return result
