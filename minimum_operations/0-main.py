78b656866b13fefff49e970e4370b61aff9f532b
Joellate <j.teta@alustudent.com>
2025-05-17 15:22:12 -0700

    task

--- minimum_operations/0-main.py
+++ minimum_operations/0-main.py
@@ -6,14 +6,14 @@ Main file for testing
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 9
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 1
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""minimum operations"""
-from math import sqrt
+"""
+Minimum Operations: Calculates the fewest number of operations needed
+to result in exactly n H characters using Copy All and Paste.
+"""
 
 
 def minOperations(n):
-
-    if n <= 1:
+    """Return minimum operations to reach n H's, else 0 if not possible."""
+    if n < 2:
         return 0
-    elif is_n_prime(n):
-        return n
-    else:
-        prime = small_prime(n)
-        if prime:
-            return prime + minOperations(int(n/prime))
 
+    operations = 0
+    factor = 2
 
-def is_n_prime(n):
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
 
-    result = True
-    for i in range(2, int(sqrt(n)) + 1):
-        if n % i == 0:
-            result = False
-    return result
+    return operations
 
-
-def small_prime(n):
-
-    result = None
-    for i in range(2, int(n/2 + 1)):
-        if n % i == 0:
-            result = i
-            break
-    return result
